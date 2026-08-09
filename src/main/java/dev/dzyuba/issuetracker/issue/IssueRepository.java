65a549d071dbce44ee1775b40de0a448ec0c0be4
Konstantin Dzyuba <dzyubaka3@gmail.com>
2026-08-09 18:54:03 +0300

    feat: list issues

--- src/main/java/dev/dzyuba/issuetracker/issue/IssueRepository.java
+++ src/main/java/dev/dzyuba/issuetracker/issue/IssueRepository.java
@@ -1,8 +1,14 @@
 package dev.dzyuba.issuetracker.issue;
 
+import dev.dzyuba.issuetracker.project.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IssueRepository extends CrudRepository<Issue, Long> {
+
+    List<Issue> findByProject(Project project);
+
 }
